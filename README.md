6b5236be77ae16b447fc3f9170791b9034f0cb2c
Hajime Yoshimori <lugia.kun@gmail.com>
2012-04-25 07:29:09 +0900

    use setting panel

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
+# -*- coding: utf-8 -*-
+
 mikutter-plugin-galateatalk
 ===========================
+
+This is a plugin for mikutter which provides to speak mentioned
+messages to you by Japanese Text-To-Speech synthesis, Galatea Talk.
+
+日本語TTSの 1 つである Galatea Talk を用いて、リプライなどのメッセージ
+を朗読してもらうための mikutter プラグインです。
+
+There is not Galatea Talk here.
+
+ここには Galatea Talk はありません。
+
+Restrictions
+============
+
+* Galatea Talk does not try to read words which it can't recognize.
+* You can use Galatea Talk with no charge, but it's "not free".
+* Galatea Talk can only recognize EUC-JP character set.
+
+* 読めない語は無理に読もうとせず読み飛ばす性格をしています。
+* Galatea Talk は総じて無料で使うことができますが、もれなく「しがらみ」
+  が付いてきます。
+* Galatea Talk は EUC-JP しか読めません。
+
+License
+=======
+
+LGPL.
+
+* Same as mikutter.
+* mikutter と同じ。
+
+mikutter
+========
+
+mikutter is one of many twitter client softwares, which is written in
+Ruby.
+
+mikutter は Ruby で書かれた、あまたあるツイッタークライアントアプリのう
+ちの 1 つです。
+
+* http://mikutter.hachune.net/
--- gtalk.rb
+++ gtalk.rb
@@ -3,10 +3,50 @@
 require 'nkf'
 
 Plugin.create :galatea_talk do
-  gtalk_bin = '/home/lugia/galatea-v3.0/SSM/gtalk'
-  conf_file = '../plugin/ssm.conf'
-  speaker = 'female01'
-  msgs = ["本日は晴天なり。発話者は #{speaker} です。"]
+  gtalk_bin = ''
+  conf_file = ''
+  speaker = ''
+  msgs = []
+
+  if UserConfig[:gtalk_conf] != nil then
+    conf_file = UserConfig[:gtalk_conf]
+  end
+  if conf_file != '' then
+    fi = open(conf_file, "r")
+    sids = fi.readlines
+    fi.close
+    sids.reject! do |m|
+      m !~ /SPEAKER-ID: /
+    end
+    sidp = {}
+    sids.each do |m|
+      n = m.gsub(/^SPEAKER-ID: *(.*)\n$/, "\\1")
+      sidp["#{n}"] = "#{n}"
+    end
+  else
+    sidp = {}
+  end
+
+  settings("Galatea Talk") do
+    settings("Galatea Talk") do
+      fileselect("Gtalk バイナリ (gtalk)", :gtalk_bin)
+      fileselect("Gtalk 設定ファイル (ssm.conf)", :gtalk_conf)
+    end
+
+    settings("SSM 設定") do
+      select("デフォルトの話者", :gtalk_speaker, sidp)
+    end
+
+    multi("開始時メッセージ", :gtalk_messages)
+    about("about",
+          {
+            :name => "mikutter + Galatea talk plugin",
+            :version => "1.0",
+            :comments => "反映には再起動が必要です",
+            :copyright => "Copyright (C) 2012 Hajime Yoshimori",
+            :authors => ["@LugiaKun"]
+          })
+  end
 
   def wait_for_msg (stream, msg)
     while 1 do
@@ -22,30 +62,51 @@ Plugin.create :galatea_talk do
     return 0
   end
 
-  rd, wr = IO.pipe
-  cpid = fork {
-    wr.close
-    gtalk = IO.popen("#{gtalk_bin} -C #{conf_file}", "r+") || raise
-    wait_for_msg(gtalk, "rep Run = LIVE\n") || raise
-    gtalk.puts "set Speaker = #{speaker}\n"
-    wait_for_msg(gtalk, "rep Speaker = #{speaker}\n") || raise
-    while msg = rd.gets do
-      gtalk.puts NKF.nkf("-We", "set Text = #{msg}\n")
-      wait_for_msg(gtalk, "rep Speak.stat = READY\n") || raise
-      gtalk.puts "set Speak = NOW\n"
-      wait_for_msg(gtalk, "rep Speak.stat = IDLE\n") || raise
-    end
-    rd.close
-    gtalk.puts "set Run = EXIT\n"
-    gtalk.close
-    exit
-  }
-  rd.close
+  @cpid = -1
+  @rd, @wr = IO.pipe
 
   onboot do
+    if UserConfig[:gtalk_bin] != nil then
+      gtalk_bin = UserConfig[:gtalk_bin]
+    end
+    if UserConfig[:gtalk_conf] != nil then
+      conf_file = UserConfig[:gtalk_conf]
+    end
+    if UserConfig[:gtalk_speaker] != nil then
+      speaker = UserConfig[:gtalk_speaker]
+    end
+    if UserConfig[:gtalk_messages] != nil then
+      msgs = UserConfig[:gtalk_messages]
+    end
+
+    if gtalk_bin == '' || conf_file == '' then
+      break
+    end
+
+    @cpid = fork {
+      @wr.close
+      gtalk = IO.popen("#{gtalk_bin} -C #{conf_file}", "r+") || raise
+      wait_for_msg(gtalk, "rep Run = LIVE\n") || raise
+      if speaker != '' then
+        gtalk.puts "set Speaker = #{speaker}\n"
+        wait_for_msg(gtalk, "rep Speaker = #{speaker}\n") || raise
+      end
+      while msg = @rd.gets do
+        gtalk.puts NKF.nkf("-We", "set Text = #{msg}\n")
+        wait_for_msg(gtalk, "rep Speak.stat = READY\n") || raise
+        gtalk.puts "set Speak = NOW\n"
+        wait_for_msg(gtalk, "rep Speak.stat = IDLE\n") || raise
+      end
+      @rd.close
+      gtalk.puts "set Run = EXIT\n"
+      gtalk.close
+      exit
+    }
+    @rd.close
+
     msgs.each do |msg|
       nmsg = msg.gsub(/\n/, " ")
-      wr.puts nmsg
+      @wr.puts nmsg
     end
   end
 
@@ -60,24 +121,37 @@ Plugin.create :galatea_talk do
         nmsg = nmsg.gsub(/@#{idname}/i, "")
       end
       nmsg = nmsg.gsub(/\n/, " ")
-      wr.puts nmsg
+      @wr.puts nmsg
     end
   end
 
-  oldmsg = ""
+  oldmsgf = ""
   onfavorite do |serv,user,msg|
-    if oldmsg == msg.body then
+    if oldmsgf != msg.body then
       nmsg = msg.body.gsub(/\n/, " ")
     else
       nmsg = "同じの"
     end
-    wr.puts user[:name] + "が「<EMPH>" + nmsg + "</EMPH>」をファボったよ。"
-    oldmsg = msg.body
+    @wr.puts user[:name] + "が「<EMPH>" + nmsg + "</EMPH>」をファボッたよ。"
+    oldmsgf = msg.body
   end
+
+#  oldmsgr = ""
+#  onretweet do |msgs|
+#    msgs.each do |msg|
+#      puts msg.body
+#      if oldmsgr != msg.body then
+#        nmsg = msg.body.gsub(/\n/, " ")
+#      else
+#        nmsg = "同じの"
+#      end
+#      @wr.puts "「<EMPH>" + nmsg + "</EMPH>」を<SPELL>RT</SPELL>したよ。"
+#      oldmsgr = msg.body
+#    end
+#  end
 
   at_exit do
-    wr.close
-    wait cpid
+    @wr.close
+    wait @cpid
   end
 end
-
